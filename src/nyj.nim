cc3707d9d5421c26fd216606046076d91cd5472c
Anuken <arnukren@gmail.com>
2021-12-29 14:16:04 -0500

    fish

--- src/nyj.nim
+++ src/nyj.nim
@@ -1,6 +1,12 @@
-import ecs, fau/presets/[basic, effects], fau/util/util, math
+import ecs, fau/presets/[basic, effects], fau/util/util, math, random
 
 static: echo staticExec("faupack -p:../assets-raw/sprites -o:../assets/atlas")
+
+#palette
+const
+  col1 = %"2c2352"
+  col2 = %"4f6b7a"
+  col3 = %"a7c6c5"
 
 const 
   scl = 4f
@@ -10,9 +16,9 @@ var player: EntityRef
 
 defineEffects:
   bubble(lifetime = 1f):
-    let off = vec2(0f, e.fin.powout(3f) * 4f)
-    poly(e.pos + off, 10, 4f + 4f * e.fin.pow(4f), stroke = 2f * e.fout)
-    fillCircle(e.pos + vec2(1.1f) + off, 2f * e.fout)
+    let off = vec2(0f, e.fin.powout(3f) * 7f)
+    poly(e.pos + off, 10, 4f + 4f * e.fin.pow(4f), stroke = 2f * e.fout, color = col3)
+    fillCircle(e.pos + vec2(1.1f) + off, 2f * e.fout, color = col3)
 
 registerComponents(defaultComponentOptions):
   type
@@ -29,7 +35,7 @@ sys("move", [Player, Vel, Pos]):
   all:
     let vec = vec2(axis(keyA, keyD), axis(keyS, keyW)).lim(1f) * playerSpeed * fau.delta
     if vec.len > 0:
-      item.vel.rot = aapproach(item.vel.rot, vec.angle, 3f * fau.delta)
+      item.vel.rot = aapproach(item.vel.rot, vec.angle, 6f * fau.delta)
     
     let base = vec.angled(item.vel.rot)
 
@@ -38,12 +44,8 @@ sys("move", [Player, Vel, Pos]):
 
     if vec.len > 0:
       item.vel.moveTime += fau.delta
-      incTimer(item.vel.bub, 7f / 60f):
-        effectBubble(item.pos.vec2 + randVec(7f))
-
-    #if item.vel.bub >= 1f:
-    #  effectBubble(item.pos.vec2 + randVec(5f))
-    #  item.vel.bub = 0f
+      incTimer(item.vel.bub, 3f / 60f):
+        effectBubble(item.pos.vec2 + vec2l(item.vel.rot, 11f) + randVec(7f))
 
 sys("draw", [Main]):
   fields:
@@ -56,10 +58,10 @@ sys("draw", [Main]):
   start:
     if keyEscape.tapped: quitApp()
 
-    sys.buffer.clear(colorBlack)
+    sys.buffer.clear(col1)
     sys.buffer.resize(fau.sizei div scl.int)
     
-    fau.cam.update(fau.size / scl)
+    fau.cam.update(fau.size / scl, player.fetch(Pos).vec2)
     fau.cam.use()
 
     drawBuffer(sys.buffer)
@@ -67,9 +69,30 @@ sys("draw", [Main]):
   finish:
     discard
 
+sys("fish", [Main]):
+  start:
+    let p = "fish".patch
+    const amount = 50
+    
+    let view = fau.cam.viewport.grow(fau.cam.size.x)
+    let r = 1000f
+
+    var ra = initRand(1)
+
+    for i in 0..<amount:
+      let scl = ra.rand(1f..4f) * 3f
+
+      var pos = vec2(ra.rand(-r..r), ra.rand(-r..r)) + vec2(fau.time * scl, sin(fau.time, ra.rand(0.7f..1.5f), ra.rand(0f..4f)))
+      pos -= view.pos
+      pos = vec2(pos.x.emod view.w, pos.y.emod view.h)
+      pos += view.pos
+
+      draw(p, pos, scl = vec2(-1f + sin(fau.time, ra.rand(0.1f..0.3f), 0.06f), 1f))
+
+
 sys("drawPlayer", [Player, Pos, Vel]):
   all:
-    draw("player".patch, item.pos.vec2, rotation = item.vel.rot - 90f.rad, scl = vec2(1f, 1f + sin(item.vel.moveTime, 0.1f, 0.13f)))
+    draw("player".patch, item.pos.vec2, rotation = item.vel.rot - 90f.rad, scl = vec2(1f, 1f + sin(item.vel.moveTime, 0.1f, 0.13f)), color = col3)
 
 makeEffectsSystem()
 
