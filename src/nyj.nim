dc58eda82a8611721e9658ab8b5b24f2806c76fe
Anuken <arnukren@gmail.com>
2021-12-29 11:53:05 -0500

    basic movement, drawing

--- src/nyj.nim
+++ src/nyj.nim
@@ -2,27 +2,59 @@ import ecs, fau/presets/[basic, effects]
 
 static: echo staticExec("faupack -p:../assets-raw/sprites -o:../assets/atlas")
 
-const scl = 4.0
+const 
+  scl = 4f
+  playerSpeed = 45f
+
+var player: EntityRef
 
 registerComponents(defaultComponentOptions):
   type
     Vel = object
-      x, y: float32
+      x, y, rot: float32
+    
+    Player = object
 
-sys("init", [Main]):
+sys("move", [Player, Vel, Pos]):
+  all:
+    let vec = vec2(axis(keyA, keyD), axis(keyS, keyW)).lim(1f) * playerSpeed * fau.delta
+    if vec.len > 0:
+      item.vel.rot = aapproach(item.vel.rot, vec.angle, 3f * fau.delta)
+    
+    let base = vec.angled(item.vel.rot)
+
+    item.pos.x += base.x
+    item.pos.y += base.y
+
+sys("draw", [Main]):
+  fields:
+    buffer: Framebuffer
 
   init:
-    discard
+    player = newEntityWith(Vel(), Pos(), Player())
+    sys.buffer = newFramebuffer()
 
   start:
     if keyEscape.tapped: quitApp()
+
+    sys.buffer.clear()
+    sys.buffer.resize((fau.size / scl).vec2i)
     
     fau.cam.update(fau.size / scl)
     fau.cam.use()
 
-    fillPoly(vec2(0, 0), 6, 30)
+    drawBuffer(sys.buffer)
   
   finish:
     discard
 
-launchFau("nyj")
+sys("drawPlayer", [Player, Pos, Vel]):
+  all:
+    draw("player".patch, item.pos.vec2, rotation = item.vel.rot - 90f.rad)
+
+sys("endDraw", [Main]):
+  start:
+    drawBufferScreen()
+    sysDraw.buffer.blit()
+
+launchFau("New Year's Jam 2021")
